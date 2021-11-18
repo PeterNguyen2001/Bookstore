610e183dce54e69518669da56fecaaa96946357a
shoxie <doctorwrose@gmail.com>
2021-11-18 13:03:38 +0700

    fix html

--- component/Products/ProductItem.js
+++ component/Products/ProductItem.js
@@ -3,25 +3,25 @@ import { GrFormView } from "react-icons/gr";
 import { AiOutlineHeart } from "react-icons/ai";
 export default function ProductItem({ product }) {
   return (
-    <div className="border flex flex-col max-w-max font-serif text-center items-center">
+    <div className="flex flex-col items-center min-w-full text-center transition-all duration-200 border group hover:shadow-lg hover:cursor-pointer">
       <div className="relative">
-        <img className="md:w-full md:h-56" src={product.img} />
-        <div className="w-full bottom-2 left-10 absolute flex flex-row gap-3 bg-transparent">
-          <button className="rounded-full bg-gray-300">
+        <img className="md:w-full md:h-72" src={product.img} />
+        <div className="absolute flex flex-row justify-center min-w-full gap-3 bg-transparent bottom-2">
+          <button className="hidden p-2 transition-all duration-500 bg-gray-300 rounded-full group-hover:block">
             <BsCart2 />
           </button>
-          <button className="rounded-full bg-gray-300">
+          <button className="hidden p-2 transition-all duration-700 bg-gray-300 rounded-full group-hover:block">
             <GrFormView />
           </button>
-          <button className="rounded-full bg-gray-300">
+          <button className="hidden p-2 transition-all duration-1000 bg-gray-300 rounded-full group-hover:block">
             <AiOutlineHeart />
           </button>
         </div>
       </div>
 
-      <div className="border-t w-full">
+      <div className="w-full px-3 py-3 border-t">
         <div>{product.name}</div>
-        <div>{product.price}</div>
+        <div>€{product.price}</div>
       </div>
     </div>
   );
--- component/Products/ProductList.js
+++ component/Products/ProductList.js
@@ -2,10 +2,12 @@ import ProductItem from "./ProductItem"
 
 export default function ProductList({ products }) {
     return(
-        <div className="grid md:grid-cols-4 grid-cols-2 gap-3 items-center">
+        <div className="max-w-screen-xl mx-auto">
+            <div className="grid items-center grid-cols-2 gap-5 md:grid-cols-4">
             {
                 products.map((product, idx) => <ProductItem key={idx} product={product} />)
             }
+        </div>
         </div>
     )
 }
